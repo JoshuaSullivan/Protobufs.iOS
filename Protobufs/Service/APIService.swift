e8135d76a259295cc4dfc7f97f0f9660e0caa244
Joshua Sullivan <jsulliva@nerdery.com>
2017-04-03 15:49:05 -0500

    Created APIService file, but won’t move ahead with contents until I have a better idea how to structure Protobuf requests.

---
+++ Protobufs/Service/APIService.swift
@@ -0,0 +1,11 @@
+//
+//  APIService.swift
+//  Protobufs
+//
+//  Created by Joshua Sullivan on 4/3/17.
+//  Copyright © 2017 The Nerdery. All rights reserved.
+//
+
+import Foundation
+
+fileprivate let apiKey = "0e63e5f89934aad2fa5e1d76e5d7a2bd";
