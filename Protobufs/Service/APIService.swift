0bb2496123aee7316b3f4249416f0c8508081e8f
Joshua Sullivan <jsulliva@nerdery.com>
2017-04-04 10:42:52 -0500

    Beginning to flesh out the API Service.

--- Protobufs/Service/APIService.swift
+++ Protobufs/Service/APIService.swift
@@ -9,3 +9,39 @@
 import Foundation
 
 fileprivate let apiKey = "0e63e5f89934aad2fa5e1d76e5d7a2bd";
+
+class APIService {
+    
+    // MARK: Child Types and Constants
+    
+    
+    
+    // MARK: - Singleton
+    
+    private static let _shared: APIService = {
+        let basePath = "http://www.google.com/"
+        guard let url = URL(string: basePath) else {
+            fatalError("Could not create the base URL for the APIService.")
+        }
+        return APIService(baseURL: url)
+    }()
+    
+    /// The shared singleton instance of APIService.
+    class var shared: APIService {
+        return _shared
+    }
+    
+    // MARK: - Properties
+    
+    fileprivate let baseURL: URL
+    
+    // MARK: - Lifecycle
+    
+    init(baseURL: URL) {
+        self.baseURL = baseURL
+    }
+    
+    // MARK: - Sending
+    
+    // MARK: - Receiving
+}
