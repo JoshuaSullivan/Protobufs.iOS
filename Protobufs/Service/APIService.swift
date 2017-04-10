d583cca7fdcfeadd32faa68acc3df6566ae9e485
Joshua Sullivan <jsulliva@nerdery.com>
2017-04-10 14:48:45 -0500

    Connect to the server and route request/response via Protobufs.

--- Protobufs/Service/APIService.swift
+++ Protobufs/Service/APIService.swift
@@ -8,18 +8,12 @@
 
 import Foundation
 
-fileprivate let apiKey = "0e63e5f89934aad2fa5e1d76e5d7a2bd";
-
 class APIService {
-    
-    // MARK: Child Types and Constants
-    
-    
     
     // MARK: - Singleton
     
     private static let _shared: APIService = {
-        let basePath = "http://www.google.com/"
+        let basePath = "http://localhost:8080"
         guard let url = URL(string: basePath) else {
             fatalError("Could not create the base URL for the APIService.")
         }
@@ -43,5 +37,43 @@ class APIService {
     
     // MARK: - Sending
     
+    func send(message: String, completion: @escaping (Result<String>) -> Void) {
+        var proto = SimpleMessage()
+        proto.content = message
+        guard let protoData = try? proto.serializedData() else {
+            print("Failed to create protobuf for sending.")
+            completion(.failure(nil))
+            return
+        }
+        let requestURL = baseURL.appendingPathComponent("/proto")
+        var request = URLRequest(url: requestURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        request.httpMethod = "POST"
+        request.httpBody = protoData
+        URLSession.shared.dataTask(with: request) {
+            (optData, optResponse, optError) in
+            DispatchQueue.main.async {
+                if let error = optError {
+                    print("Request failed: \(error.localizedDescription)")
+                    completion(.failure(error))
+                    return
+                }
+                guard
+                    let data = optData,
+                    let sMsg = try? SimpleMessage(serializedData: data)
+                else {
+                    print("Failed to deserialize Protocol Buffer.")
+                    completion(.failure(nil))
+                    return
+                }
+                completion(.success(sMsg.content))
+            }
+        }.resume()
+    }
+    
     // MARK: - Receiving
+}
+
+enum Result<T> {
+    case success(T)
+    case failure(Error?)
 }
