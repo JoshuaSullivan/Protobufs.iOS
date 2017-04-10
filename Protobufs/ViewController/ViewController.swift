f47cf6b4767945806ae791d02d51d2860cf27957
Joshua Sullivan <jsulliva@nerdery.com>
2017-04-10 14:49:36 -0500

    Setting up the ViewController to send and receive via Protobufs.

--- Protobufs/ViewController/ViewController.swift
+++ Protobufs/ViewController/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var responseLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendTapped(sender: AnyObject?) {
+        guard
+            let message = inputField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+            !message.isEmpty
+        else {
+            return
+        }
+        APIService.shared.send(message: message) {
+            [weak self]
+            result in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let returnMessage):
+                strongSelf.responseLabel.text = returnMessage
+            case .failure(let optError):
+                if let error = optError {
+                    strongSelf.responseLabel.text = "ERROR: \(error.localizedDescription)"
+                    return
+                }
+                strongSelf.responseLabel.text = "UNKOWN ERROR"
+            }
+        }
+    }
 
 }
 
